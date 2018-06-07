4da10934d1d201e6c6f067ca5c3269d6d2dbfc45
rk <rk@RKinfoech>
2018-06-07 12:42:20 +0530

    embedded database

--- src/main/java/com/telusko/controller/IndexController.java
+++ src/main/java/com/telusko/controller/IndexController.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.support.ModelAndViewContainer;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.telusko.Response.Response;
